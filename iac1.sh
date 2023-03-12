684760b45f6fdc821397021502c2ac45960b4f8c
aguia7 <79702490+aguia7@users.noreply.github.com>
2023-03-11 22:01:50 -0300

    Update iac1.sh
    Add -c descrição dos nomes

--- iac1.sh
+++ iac1.sh
@@ -15,9 +15,9 @@ groupadd GRP_SEC
 
 echo "Criando usuários..."
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd carlos -m -c "Carlos Dio" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -c "Maria DIO" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -c "Joao DIO" -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 
 useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
